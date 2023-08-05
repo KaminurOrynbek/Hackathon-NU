5c70a940a7c2260735d07607ff861c588588112f
kaminur1 <139678086+kaminur1@users.noreply.github.com>
2023-08-05 10:53:35 +0600

    Update router.py

--- posts/router.py
+++ posts/router.py
@@ -1,15 +1,2 @@
-from typing import Optional
-from pydantic import BaseModel
 
-
-class CreatePostRequest(BaseModel):
-    username: str
-    keywords: str
-    text: str
-
-
-class EditPostRequest(BaseModel):
-    username: Optional[str] = None
-    keywords: Optional[str] = None
-    text: Optional[str] = None
     
